8a574d48ca8052384e81fb947cadaf38a36d1146
JeffBuss <turbocapslock@gmail.com>
2017-02-16 22:35:01 -0700

    Install basic dependencies

---
+++ app/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+const App = () => {
+  return (
+    <div>ClandesDine</div>
+  );
+}
+
+render(<App />, document.querySelector('.application'));
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: './app',
+  output: {
+    path: './build/bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      }
+    ]
+  },
+  devServer: {
+    contentBase: './build',
+    inline: true
+  },
+
+  resolve: {
+    extensions: ['*', '.js', '.json', '.jsx', '.css', '.scss'],
+  }
+}
