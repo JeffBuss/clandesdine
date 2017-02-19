a268f5f09d8bf22151a12eec59d02bec5a7e1eb6
JeffBuss <turbocapslock@gmail.com>
2017-02-18 21:05:41 -0700

    Set up redux devtools extension

--- app/index.jsx
+++ app/index.jsx
@@ -2,16 +2,16 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers/index';
 import App from './components/app/App';
 
 import './styles';
 import './reset';
 
-// const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, devTools);
 
 const router = (
   <Provider store={store}>
