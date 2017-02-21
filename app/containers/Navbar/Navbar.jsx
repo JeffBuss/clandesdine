761102513f92302ed35ff484e7dced3900f995ce
JeffBuss <turbocapslock@gmail.com>
2017-02-20 20:30:57 -0700

    Dropdown nav menu displaying in navbar

--- app/containers/Navbar/Navbar.jsx
+++ app/containers/Navbar/Navbar.jsx
@@ -2,6 +2,10 @@ import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router';
 
 import AppBar from 'material-ui/AppBar';
+import IconMenu from 'material-ui/IconMenu';
+import IconButton from 'material-ui/IconButton';
+import MenuItem from 'material-ui/MenuItem';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 
 const buttonStyle = { color: 'white' };
 
@@ -10,11 +14,21 @@ export default class Navbar extends Component {
     return (
       <AppBar
         title={
-          <Link to='/' style={buttonStyle}>
+          <Link to="/" style={buttonStyle}>
             ClandesDine
           </Link>
         }
-        showMenuIconButton={false}
+        iconElementLeft={
+          <IconMenu
+            iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
+            anchorOrigin={{ horizontal: 'left', vertical: 'top' }}
+            targetOrigin={{ horizontal: 'left', vertical: 'top' }}
+          >
+            <MenuItem primaryText="About" />
+            <MenuItem primaryText="Media" />
+            <MenuItem primaryText="FAQs" />
+            <MenuItem primaryText="Contact" />
+          </IconMenu>}
       />
     );
   }
