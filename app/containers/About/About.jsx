c7aa5ccfe2aa9a1cdf28fb11ac388b2d9f9d8fcf
JeffBuss <turbocapslock@gmail.com>
2017-03-06 12:49:43 -0700

    Finish FAQs and About pages

--- app/containers/About/About.jsx
+++ app/containers/About/About.jsx
@@ -1,10 +1,26 @@
 import React, { Component, PropTypes } from 'react';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
 
 export default class About extends Component {
   render() {
     return (
       <div>
-        About page
+        <Card>
+          <CardMedia>
+            <img src="http://i.imgur.com/Yt0yN4R.jpg" />
+          </CardMedia>
+          <CardHeader> About Clandesdine</CardHeader>
+          <CardText>Lorem ipsum dolor sit amet, congue regione referrentur ne eam. Ea ius habeo fabellas efficiendi, sea id adhuc epicurei. Referrentur reprehendunt no mei, an solet aliquando nam, no duo alii veri tacimates. Ei duo dicat dictas detraxit.
+
+Fabellas suavitate sadipscing est ea, sed enim nostrud eu, atomorum explicari his ex. Agam labitur eos te. An latine maluisset vix. Est quem dicat suscipit id. Eu falli integre sea, tollit deserunt at duo, tamquam mediocritatem ex ius. Nihil detracto per in, alienum contentiones vix cu, an quo assum doming.
+
+Copiosae complectitur id eum, quem erat definiebas per ne. In est etiam laoreet, cu cum legimus vulputate. Omnes omittam vix eu. Vel ridens persecuti cu, summo posidonium dissentiet sea ut, illum audire cu pri.
+
+Duo ne ipsum fabulas menandri, nam sanctus officiis torquatos ei, natum harum delectus ex has. Error inimicus id vim, et nam nibh magna. At tation suscipit his, pro ei suas meis ferri. Ei ludus quidam vix, cum euismod mentitum scribentur ei, et vel tamquam praesent. Sit te cibo prompta numquam.
+
+Mea mundi aliquam dolores ei, eum novum docendi no, duo ne iisque adversarium. At falli menandri quaerendum nec, ne aperiri nostrum luptatum mel. Mei ocurreret disputationi ut, est amet propriae interesset eu. Ut eos error exerci voluptua, ex sed mazim iriure complectitur.</CardText>
+        </Card>
       </div>
     );
   }
--- app/containers/Faqs/Faqs.jsx
+++ app/containers/Faqs/Faqs.jsx
@@ -1,10 +1,26 @@
 import React, { Component, PropTypes } from 'react';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
 
-export default class Faqs extends Component {
+export default class About extends Component {
   render() {
     return (
       <div>
-        Faqs page
+        <Card>
+          <CardTitle>Frequently Asked Questions</CardTitle>
+          <CardTitle>Question Lorem ipsum dolor sit amet</CardTitle>
+          <CardText>Answer Ridens facilisi ex vix, omnesque inimicus urbanitas te vis, his alia vivendo epicuri te. Ea quo eirmod sensibus rationibus, equidem lucilius ex vim. Idque nemore docendi ne nam, ex vel consulatu reprimique appellantur. Te eam tale adolescens, vis at autem postea eripuit.</CardText>
+          <CardTitle>Question Lorem ipsum dolor sit amet</CardTitle>
+          <CardText>Answer Ridens facilisi ex vix, omnesque inimicus urbanitas te vis, his alia vivendo epicuri te. Ea quo eirmod sensibus rationibus, equidem lucilius ex vim. Idque nemore docendi ne nam, ex vel consulatu reprimique appellantur. Te eam tale adolescens, vis at autem postea eripuit.</CardText>
+          <CardTitle>Question Lorem ipsum dolor sit amet</CardTitle>
+          <CardText>Answer Ridens facilisi ex vix, omnesque inimicus urbanitas te vis, his alia vivendo epicuri te. Ea quo eirmod sensibus rationibus, equidem lucilius ex vim. Idque nemore docendi ne nam, ex vel consulatu reprimique appellantur. Te eam tale adolescens, vis at autem postea eripuit.</CardText>
+          <CardTitle>Question Lorem ipsum dolor sit amet</CardTitle>
+          <CardText>Answer Ridens facilisi ex vix, omnesque inimicus urbanitas te vis, his alia vivendo epicuri te. Ea quo eirmod sensibus rationibus, equidem lucilius ex vim. Idque nemore docendi ne nam, ex vel consulatu reprimique appellantur. Te eam tale adolescens, vis at autem postea eripuit.</CardText>
+          <CardTitle>Question Lorem ipsum dolor sit amet</CardTitle>
+          <CardText>Answer Ridens facilisi ex vix, omnesque inimicus urbanitas te vis, his alia vivendo epicuri te. Ea quo eirmod sensibus rationibus, equidem lucilius ex vim. Idque nemore docendi ne nam, ex vel consulatu reprimique appellantur. Te eam tale adolescens, vis at autem postea eripuit.</CardText>
+          <CardTitle>Question Lorem ipsum dolor sit amet</CardTitle>
+          <CardText>Answer Ridens facilisi ex vix, omnesque inimicus urbanitas te vis, his alia vivendo epicuri te. Ea quo eirmod sensibus rationibus, equidem lucilius ex vim. Idque nemore docendi ne nam, ex vel consulatu reprimique appellantur. Te eam tale adolescens, vis at autem postea eripuit.</CardText>
+        </Card>
       </div>
     );
   }
